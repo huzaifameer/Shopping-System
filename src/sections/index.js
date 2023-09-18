4f339905482005c6210e7fee8ee8b6e6ecb87009
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-09-18 20:09:00 +0530

    commit6

---
+++ src/sections/index.js
@@ -0,0 +1,13 @@
+import Hero from './sections/Hero';
+import Services from './sections/Services';
+import CustomerReviews from './sections/CustomerReviews';
+import PopularProducts from './sections/PopularProducts';
+import Footer from './sections/Footer';
+import SpecialOffer from './sections/SpecialOffer';
+import Subscribe from './sections/Subscribe';
+import SuperQuality from './sections/SuperQuality';
+
+export{
+    Hero,Services,CustomerReviews,PopularProducts,
+    Footer,SpecialOffer,Subscribe,SuperQuality
+}

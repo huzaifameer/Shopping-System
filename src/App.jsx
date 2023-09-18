6f28f4f371a10dea9fc1612affad7f7b95bceeeb
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-09-18 19:22:54 +0530

    commit2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
-
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () =>(
+  <main className="relative">
+  Nav
+  <section className="xl:padding-1 wide:padding-r padding-b">
+    Hero
+  </section>
+  <section className="padding">
+    PopularProduct
+  </section>
+  <section className="padding">
+    SuperQuality
+  </section>
+  <section className="padding-x py-10">
+    Services
+  </section>
+  <section className="padding">
+    SpecialOffers
+  </section>
+  <section className="bg-pale-blue padding">
+    Customer Reviews
+  </section>
+  <section className="padding-x sm:py-32 py-16 w-full">
+    Subscribe
+  </section>
+  <section className="padding">
+    Footer
+  </section>
+  </main>
+);
+export default App;

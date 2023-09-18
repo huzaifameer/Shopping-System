005e9267de851adc82b4d49a68e9eb5d5243b50a
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-09-18 23:14:22 +0530

    commit10

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,31 @@
-
-
-const Button = () => {
-  return (
-    <button className="">
-        Click Me
-    </button>
-  )
-}
-
-export default Button
+const Button = ({
+    label,
+    iconURL,
+    backgroundColor,
+    textColor,
+    borderColor,
+    fullWidth,
+  }) => {
+    return (
+      <button
+        className={`flex justify-center items-center gap-2 px-7 py-4 border-2 font-montserrat text-lg leading-none
+        ${
+          backgroundColor
+            ? `${backgroundColor} ${textColor} ${borderColor}`
+            : "bg-blue-800 text-white border-blue-900"
+        } rounded-full ${fullWidth && "w-full"}`}
+      >
+        {label}
+  
+        {iconURL && (
+          <img
+            src={iconURL}
+            alt='arrow right icon'
+            className='ml-2 rounded-full bg-white w-5 h-5'
+          />
+        )}
+      </button>
+    );
+  };
+  
+  export default Button;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,23 +1,21 @@
+import { arrowRight } from "../assets/icons"
 import Button from "../components/Button"
 
 
 const Hero = () => {
   return (
-    <section id="home" className="w-full flex
-    xl:flex-row flex-col justify-center min-h-screen
-    gap-10 max-container">
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p>Our Product Collection</p>
         <h1>
           <span>The New Arrival</span><br />
-          <span>Nike</span>
-          <span>Shoes</span>
+          <span>Nike</span>Shoes
         </h1>
         <p>Elevate your wardrobe with our exquisite designs, a canvas for your self-expression.</p>
-        <Button />
+        <Button label='Shop now' iconURL={arrowRight} />
       </div>
     </section>
   )
 }
 
-export default Hero
+export default Hero;
